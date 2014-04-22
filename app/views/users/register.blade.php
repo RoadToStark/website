fd5693f999724602ab20f26d53d0864aed4a26de
lucasbedout <lucas.bedout@free.fr>
2014-04-22 12:12:02 -0400

    design register

--- app/views/users/register.blade.php
+++ app/views/users/register.blade.php
@@ -1,5 +1,9 @@
+@extends('layouts.default')
+
+@section('content')
+
 {{ Form::open(array('url'=>'/register', 'id'=>'form-signup')) }}
-    <h1 class="title">Enregistrement</h2>
+    <h1 class="title">Enregistrement</h1>
 	   <ul>
         @foreach($errors->all() as $error)
             <li>{{ $error }}</li>
@@ -10,9 +14,14 @@
     {{ Form::text('firstname', null, array('class'=>'user-form', 'placeholder'=>'Prénom')) }}
     {{ Form::text('lastname', null, array('class'=>'user-form', 'placeholder'=>'Nom')) }}
     {{ Form::email('email', null, array('class'=>'user-form', 'placeholder'=>'Adresse mail')) }}
-    {{ Form::password('password', array('class'=>'user-form-password', 'placeholder'=>'Mot de passe')) }}
-    {{ Form::password('password_confirmation', array('class'=>'user-form-password', 'placeholder'=>'Confirmation mot de passe')) }}
+    <div class="user-passwords">
+    	{{ Form::password('password', array('class'=>'user-form-password', 'placeholder'=>'Mot de passe')) }}
+		{{ Form::password('password_confirmation', array('class'=>'user-form-password', 'placeholder'=>'Confirmation mot de passe')) }}
+    </div>
+    
     {{ Form::text('description', null, array('class'=>'user-form-description', 'placeholder'=>'A propos de vous...')) }}
- 
-    {{ Form::submit('Register', array('class'=>'btn btn-large btn-primary btn-block'))}}
+    {{ Form::submit('Register', array('class'=>'submit-user'))}}
+    
 {{ Form::close() }}
+
+@stop
