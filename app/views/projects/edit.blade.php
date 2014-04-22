68bffaed80a6044ed74620cb2235c1d00eb14d66
lucasbedout <lucas.bedout@free.fr>
2014-04-22 11:41:35 -0400

    fixed editor layout

--- app/views/projects/edit.blade.php
+++ app/views/projects/edit.blade.php
@@ -1,25 +1,52 @@
-<h1>Edit a Project</h1>
+@extends('layouts.default')
 
-<!-- if there are creation errors, they will show here -->
+@section('content')
+
 {{ HTML::ul($errors->all()) }}
 
-{{ Form::model($project, array('route' => array('projects.update', $project->id), 'method' => 'PUT')) }}
+<div id="create-project">
+	{{ Form::model($project, array('route' => array('projects.update', $project->id), 'method' => 'PUT')) }}
 
-	<div class="form-group">
-		{{ Form::label('name', 'Name') }}
-		{{ Form::text('name', null, array('class' => 'form-control')) }}
-	</div>
+		{{ Form::text('name', null, array('class' => 'title', 'placeholder' => 'Nom du projet', 'required' => 'required')) }}
 
-	<div class="form-group">
-		{{ Form::label('description', 'Description') }}
-		{{ Form::text('description', null, array('class' => 'form-control')) }}
-	</div>
-    
-    <div class="form-group">
-		{{ Form::label('presentation', 'Présentation') }}
-		{{ Form::textarea('presentation', null, array('class' => 'form-control')) }}
-	</div>
+		{{ Form::text('description', null, array('class' => 'description', 'placeholder' => 'Courte description du projet', 'required' => 'required')) }}
+		
+		<div class="features">	
+			<section class="editor">
+				<div class="outer">
+					<div class="editorwrap">
+						<section class="entry-markdown">
+							<header class="floatingheader">
+								&nbsp;&nbsp; Présentation détaillée 
+							</header>
+							<section class="entry-markdown-content">
+								{{ Form::textarea('presentation', null, array('class' => 'form-control', 'required' => 'required')) }}
+							</section>
+						</section>
+						<section class="entry-preview active">
+							<header class="floatingheader">
+							  &nbsp;&nbsp; Rendu <span class="entry-word-count">0 mots</span>
+							</header>
+							<section class="entry-preview-content">
+								<div class="rendered-markdown"></div>
+							</section>
+						</section>
+					</div>
+				</div>
+			</section>
+			
+		</div>
+		<div class="submit">
+			{{ Form::submit('Créer le projet', array('class' => 'btn btn-dark')) }}
+		</div>
+	
+		{{ Form::close() }}
+</div>
+<script>
+	$(document).ready(function () {
+	    $(".editor").ghostDown();
+	});
+</script>
 
-	{{ Form::submit('Edit the Project!') }}
 
-{{ Form::close() }}
+@stop
